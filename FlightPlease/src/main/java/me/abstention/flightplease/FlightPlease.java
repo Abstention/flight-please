72f7758e1493b1a39802f69aee5bade57680dc04
Abstention <31156957+Abstention@users.noreply.github.com>
2021-02-14 16:46:41 +0400

    Add files via upload
    Added CONFIG file. Can change /fly messages.

--- FlightPlease/src/main/java/me/abstention/flightplease/FlightPlease.java
+++ FlightPlease/src/main/java/me/abstention/flightplease/FlightPlease.java
@@ -2,14 +2,19 @@ package me.abstention.flightplease;
 
 import me.abstention.flightplease.commands.FlightCommand;
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class FlightPlease extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         System.out.println(ChatColor.GREEN + "FlightPlease has been ENABLED!");
-        getCommand("fly").setExecutor(new FlightCommand());
-    }
-}
+        Objects.requireNonNull(getCommand("fly")).setExecutor(new FlightCommand());
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
+    }}
--- FlightPlease/src/main/java/me/abstention/flightplease/commands/FlightCommand.java
+++ FlightPlease/src/main/java/me/abstention/flightplease/commands/FlightCommand.java
@@ -1,12 +1,16 @@
 package me.abstention.flightplease.commands;
 
+import me.abstention.flightplease.FlightPlease;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 public class FlightCommand implements CommandExecutor {
+
+    Plugin plugin = FlightPlease.getPlugin(FlightPlease.class);
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -15,11 +19,15 @@ public class FlightCommand implements CommandExecutor {
             if(player.hasPermission("flightplease.fly")){
                 if(player.getAllowFlight()) {
                     player.setAllowFlight(false);
-                    player.sendMessage(ChatColor.RED + "Flight has been disabled.");
+                    String FlightDisableMessage = plugin.getConfig().getString("FlightDisableMessage");
+                    assert FlightDisableMessage != null;
+                    player.sendMessage(FlightDisableMessage);
                 }
                 else {
                     player.setAllowFlight(true);
-                    player.sendMessage(ChatColor.GOLD + "Flight has been enabled.");
+                    String FlightEnableMessage = plugin.getConfig().getString("FlightEnableMessage");
+                    assert FlightEnableMessage != null;
+                    player.sendMessage(FlightEnableMessage);
                 }
             }else{
                 player.sendMessage(ChatColor.RED + "You do not have the required permissions.");
